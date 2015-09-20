4932ec8f663422006e30b5da1a2158ac5d0a5da9
sazuxerox <sazzadhossain@live.com>
2015-09-21 00:27:02 +0600

    Initial Commit. This commits holds the service, its host in the same folder and client in a different folder

---
+++ CompanyClient/CompanyClient/CompanyClientPage.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CompanyClientPage.aspx.cs" Inherits="CompanyClient.CompanyClientPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family: verdana">
+        <asp:Button ID="PublicInfoButton" runat="server" Text="Get Public Info" OnClick="PublicInfoButton_Click" /><br/><br/>
+        <asp:Label ID="PublicInfoLabel" runat="server"></asp:Label>
+    </div>
+       
+
+     <div style="font-family: verdana">
+         <br />
+        <asp:Button ID="ConfidentialInfoButton" runat="server" Text="Get Confidential Info" OnClick="ConfidentialInfoButton_Click" /><br/><br/>
+        <asp:Label ID="ConfidentialInfoLabel" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CompanyClient/CompanyClient/CompanyClientPage.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CompanyClient.CompanyService;
+
+namespace CompanyClient
+{
+    public partial class CompanyClientPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PublicInfoButton_Click(object sender, EventArgs e)
+        {
+            var client = new CompanyPublicServiceClient("BasicHttpBinding_ICompanyPublicService");
+            PublicInfoLabel.Text = client.GetPublicInformation();
+        }
+
+        protected void ConfidentialInfoButton_Click(object sender, EventArgs e)
+        {
+            var client = new CompanyConfidentialServiceClient("NetTcpBinding_ICompanyConfidentialService");
+            ConfidentialInfoLabel.Text = client.GetConfidentialInformation();
+        }
+    }
+}
